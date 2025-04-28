66629cf6934532d1f17f0068bc2d5b672b3965e7
VEE\LENOVO <kimthanh932000@gmail.com>
2025-04-29 01:53:30 +0800

    Add GlobalUsing to QuizApp.Models

---
+++ QuizApp.Models/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+//global using QuizApp.Models.Entities;
+global using QuizApp.Models.Entities.Base;
+global using QuizApp.Models.Entities.Configuration;
