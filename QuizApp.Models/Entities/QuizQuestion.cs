e687112dd39124a5a3fc417f9541be21c40adc1b
VEE\LENOVO <kimthanh932000@gmail.com>
2025-04-29 01:54:09 +0800

    Add model QuizQuestion

---
+++ QuizApp.Models/Entities/QuizQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizApp.Models.Entities
+{
+    [EntityTypeConfiguration(typeof(QuizQuestionConfiguration))]
+    public class QuizQuestion : BaseEntity
+    {
+        public int QuizId { get; set; }
+        public Quiz QuizNavigation { get; set; }
+        public int QuestionId { get; set; }
+        public Question QuestionNavigation { get; set; }
+    }
+}
