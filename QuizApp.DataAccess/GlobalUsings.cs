1dc0d01c0ae35de1fdbf2d7804282bf0c7fbd143
VEE\LENOVO <kimthanh932000@gmail.com>
2025-05-01 15:25:57 +0800

    [QuizApp.DataAccess] Add GlobalUsings

--- QuizApp.DataAccess/GlobalUsings.cs
+++ QuizApp.DataAccess/GlobalUsings.cs
@@ -1,2 +1,9 @@
-﻿global using QuizApp.Models.Entities;
+﻿global using Microsoft.EntityFrameworkCore;
+
+global using QuizApp.Models.Entities;
 global using QuizApp.Models.Entities.Configuration;
+
+global using QuizApp.DataAccess.Data;
+global using QuizApp.DataAccess.Repos.Interfaces;
+
+
